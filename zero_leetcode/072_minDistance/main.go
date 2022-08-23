60256764e9969aab7429f6ec1bd9fc333cee2ccb
Ww2Zero <ww-haha@foxmail.com>
2022-08-23 10:12:53 +0800

    feat(072_minDistance): add some debug info

--- zero_leetcode/072_minDistance/main.go
+++ zero_leetcode/072_minDistance/main.go
@@ -22,7 +22,14 @@ func minDistance(word1 string, word2 string) int {
 			if word1[i-1] == word2[j-1] {
 				dp[i][j] = dp[i-1][j-1]
 			} else {
-				dp[i][j] = min(dp[i][j-1]+1, dp[i-1][j]+1, dp[i-1][j-1]+1)
+				//
+				dp[i][j] = min(
+					// 增加
+					dp[i][j-1]+1,
+					// 删除
+					dp[i-1][j]+1,
+					// 替换
+					dp[i-1][j-1]+1)
 			}
 		}
 	}
