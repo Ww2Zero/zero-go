c8b496ad1ee4a8261c1becce903c1fd8bcae3374
Ww2Zero <ww-haha@foxmail.com>
2022-08-27 17:27:36 +0800

    feat(226_invertTree): add leetcode solution

--- zero_leetcode/226_invertTree/main.go
+++ zero_leetcode/226_invertTree/main.go
@@ -22,4 +22,30 @@ func invertTree(root *TreeNode) *TreeNode {
 	return root
 }
 
+// invertTreeByLevel
+//Time complexity: O(n)
+func invertTreeByLevel(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	var q []*TreeNode
+	q = append(q, root)
+	for len(q) > 0 {
+		var p []*TreeNode
+		for i := 0; i < len(q); i++ {
+			node := q[i]
+			node.Left, node.Right = node.Right, node.Left
+
+			if node.Left != nil {
+				p = append(p, node.Left)
+			}
+			if node.Right != nil {
+				p = append(p, node.Right)
+			}
+		}
+		q = p
+	}
+	return root
+}
+
 //leetcode submit region end(Prohibit modification and deletion)
