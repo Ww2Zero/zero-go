5c8bf053b580fb437fc2aa1ac645ed972832739f
Ww2Zero <ww-haha@foxmail.com>
2022-08-27 17:48:21 +0800

    feat(110_isBalanced): add leetcode solution

---
+++ zero_leetcode/110_isBalanced/main.go
@@ -0,0 +1,46 @@
+package _10_isBalanced
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if !isBalanced(root.Left) || !isBalanced(root.Right) {
+		return false
+	}
+
+	lh := maxDepth(root.Left) + 1
+	rh := maxDepth(root.Right) + 1
+
+	if abs(lh-rh) > 1 {
+		return false
+	}
+	return true
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
