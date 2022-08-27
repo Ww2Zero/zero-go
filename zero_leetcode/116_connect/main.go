1bdf0395f41f3c8fecfd06f1c05ac1a837cd496e
Ww2Zero <ww-haha@foxmail.com>
2022-08-27 17:04:35 +0800

    feat(116_connect): add leetcode solution

--- zero_leetcode/116_connect/main.go
+++ zero_leetcode/116_connect/main.go
@@ -38,4 +38,37 @@ func connectTwoHelper(n1, n2 *Node) {
 	connectTwoHelper(n1.Right, n2.Left)
 }
 
+// connectByLevel 通过层序遍历的方式
+func connectByLevel(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	var res [][]*Node
+	var q []*Node
+
+	q = append(q, root)
+
+	for len(q) > 0 {
+		var p, c []*Node
+		for i := 0; i < len(q); i++ {
+			node := q[i]
+			c = append(c, node)
+			if node.Left != nil {
+				p = append(p, node.Left)
+			}
+			if node.Right != nil {
+				p = append(p, node.Right)
+			}
+		}
+		res = append(res, c)
+		q = p
+	}
+	for i := 0; i < len(res); i++ {
+		for j := 0; j < len(res[i])-1; j++ {
+			res[i][j].Next = res[i][j+1]
+		}
+	}
+	return root
+}
+
 //leetcode submit region end(Prohibit modification and deletion)
